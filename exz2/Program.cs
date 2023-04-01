4cff5aea4f34dfad036714d126a2f79734138409
stas <point.blank.point@bk.ru>
2023-04-01 16:11:27 +0300

    exz2 done

--- exz2/Program.cs
+++ exz2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int ax = ReadInt ("Координата х первой точки");
+int ay = ReadInt ("Координата y первой точки");
+int az = ReadInt ("Координата z первой точки");
+int bx = ReadInt ("Координата х второй точки");
+int by = ReadInt ("Координата y второй точки");
+int bz = ReadInt ("Координата z второй точки");
+
+int koordX = (ax - bx);
+int koordY = (ay - by);
+int koordZ = (az - bz);
+double W = Math.Sqrt(Math.Pow(koordX,2) + Math.Pow(koordY,2) + Math.Pow(koordZ,2));
+System.Console.Write($"Показать результат {W:f2}");
